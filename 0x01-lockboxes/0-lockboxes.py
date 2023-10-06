c89835994729bad605655ca7c7f7c1c5efffcdfd
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-06 08:37:15 +0300

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened."""
+    if boxes is None or boxes == []:
+        return False
+    else:
+        keys = [0]
+        for key in keys:
+            for new_key in boxes[key]:
+                if new_key not in keys and new_key < len(boxes):
+                    keys.append(new_key)
+        if len(keys) == len(boxes):
+            return True
+        else:
+            return False
