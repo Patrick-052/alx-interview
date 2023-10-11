275c5074363caf191a83cde1c9ffedc1d9918cc0
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-11 18:53:26 +0300

    Factors and Primes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,30 +1,52 @@
 #!/usr/bin/python3
-""" Minimum Operations Interview Question """
-
-
-# def minOperations(n):
-#     """Finds the minimum number of operations needed to
-#     result in exactly n H characters in the file."""
-#     if n <= 1 or type(n) is not int:
-#         return 0
-
-#     operations = 0
-#     divisor = 2
-
-#     while divisor * divisor <= n:
-#         while n % divisor == 0:
-#             operations += divisor
-#             n //= divisor
-#         divisor += 1
-
-#     # If n is a prime number greater than 1
-#     if n > 1:
-#         operations += n
-
-#     return operations
+# Minimum Operations Interview Question
+"""Overall Produre i used:
+    1. Check if n is less than or equal to 1, return 0
+    2. Check if n is not an integer, return 0
+    3. Initialize operations to 0
+    4. *While  - Initialize factor to 2
+               - check if factor * factor is less than or equal
+                 to n
+               - check if n is divisible by factor
+               - if yes, add factor to operations and divide n by factor
+                 else increment factor by 1
+               - if n is a prime number greater than 1, add n to
+                 operations
+               - return operations
+        *For - Initialize i to 2
+             - check if i is less than or equal to n
+             - check if n is divisible by i
+             - if yes, add i to operations and divide n by i
+             - return operations
+    General:
+        Smallest factor(i) of a number(n) must be:
+            - i <= sqrt(n)
+            - i * i <= n
+"""
 
 
 def minOperations(n):
+    """Finds the minimum number of operations needed to
+    result in exactly n H characters in the file."""
+    if n <= 1 or type(n) is not int:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while factor * factor <= n:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    if n > 1:
+        operations += n
+
+    return operations
+
+
+def minOperations_1(n):
     """Finds the minimum number of operations needed to
     result in exactly n H characters in the file."""
     if n <= 1 or type(n) is not int:
@@ -34,6 +56,4 @@ def minOperations(n):
         while n % i == 0:
             operations += i
             n = n / i
-    if n > 1:
-        operations += n
     return operations
