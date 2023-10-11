98793f25ed1402ab6144ddb26537f1e3372b94ce
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-11 17:28:52 +0300

    Factors and primes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,9 +7,30 @@ def minOperations(n):
     result in exactly n H characters in the file."""
     if n <= 1 or type(n) is not int:
         return 0
+
     operations = 0
-    for i in range(2, n + 1):
-        while n % i == 0:
-            operations += i
-            n = n / i
+    divisor = 2
+
+    while divisor * divisor <= n:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    # If n is a prime number greater than 1
+    if n > 1:
+        operations += n
+
     return operations
+
+# def minOperations(n):
+#     """Finds the minimum number of operations needed to
+#     result in exactly n H characters in the file."""
+#     if n <= 1 or type(n) is not int:
+#         return 0
+#     operations = 0
+#     for i in range(2, n + 1):
+#         while n % i == 0:
+#             operations += i
+#             n = n / i
+#     return operations
