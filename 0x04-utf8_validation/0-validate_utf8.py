354d7c216139a32d6231ca2ce7e6924990305a44
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-28 23:36:34 +0300

    BIt changing

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,25 +2,11 @@
 """ UTF-8 Validation """
 
 
-def validUTF8(data) -> bool:
-    """
-    Returns True if data is a valid UTF-8 encoding, else return False
-    :param data:
-    :return:
-    """
-    num_bytes = 0
-    for byte in data:
-        mask = 1 << 7
-        if not num_bytes:
-            while byte & mask:
-                num_bytes += 1
-                mask >>= 1
-            if not num_bytes:
-                continue
-            if num_bytes == 1 or num_bytes > 4:
-                return False
-        else:
-            if byte >> 6 != 0b10:
-                return False
-        num_bytes -= 1
-    return num_bytes == 0
+def validUTF8(data):
+    """ determines if a given data set represents a valid UTF-8 encoding """
+    bytez = 128
+    Bytes = 1920
+    for i in data:
+        if i >= bytez and i <= Bytes:
+            return False
+    return True
