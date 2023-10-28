4ab750545787a985266049a84f436e0bdd14ef33
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-29 01:03:57 +0300

    changes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 """ UTF-8 Validation """
 
-
 def validUTF8(data):
     """ determines if a given data set represents a valid UTF-8 encoding """
-    bytez = 128
-    Bytes = 1920
-    for i in data:
-        if i >= bytez and i <= Bytes:
-            return False
-    return True
+    n_bytes = 0
+    for num in data:
+        byte = num.to_bytes(1, 'big')
+        bits = format(byte[0], '#010b')[-8:]
+        if n_bytes == 0:
+            if bits[0] == '0':
+                continue
+            while bits[0] == '1':
+                n_bytes += 1
+                bits = bits[1:]
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if not (bits[0] == '1' and bits[1] == '0'):
+                return False
+        n_bytes -= 1
+    return n_bytes == 0
