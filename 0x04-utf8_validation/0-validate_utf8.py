5e756962348d2a5efd06d7c1859a160bc78b3ba0
Patrick-052 <muiruripatrick052@gmail.com>
2024-02-02 09:41:05 +0300

    Update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
-"""
-UTF-8 Validation
-"""
+""" UTF-8 Validation """
 
 
 def validUTF8(data) -> bool:
-    """
-    Returns True if data is a valid UTF-8 encoding, else return False
-    :param data:
-    :return:
-    """
-    num_bytes = 0
-    for byte in data:
-        mask = 1 << 7
-        if not num_bytes:
-            while byte & mask:
-                num_bytes += 1
-                mask >>= 1
-            if not num_bytes:
-                continue
-            if num_bytes == 1 or num_bytes > 4:
+    """ Returns True if data is a valid UTF-8 encoding, else return False """
+    encoded = []
+    for i in data:
+        if 0 <= i <= 255:
+            enc = i.to_bytes((i.bit_length() + 7) // 8, 'big') or b'\0'
+            enc_bin = ''.join(f'{b:08b}' for b in enc)
+            encoded.append(enc_bin)
+        else:
+            return False
+
+    i = 0
+    while i < len(encoded):
+        if encoded[i].startswith('0'):
+            i += 1
+        elif encoded[i].startswith('110'):
+            if i + 1 < len(encoded) and encoded[i + 1].startswith('10'):
+                i += 2
+            else:
+                return False
+        elif encoded[i].startswith('1110'):
+            if i + 2 < len(encoded) and encoded[i + 1].startswith(
+                    '10') and encoded[i + 2].startswith('10'):
+                i += 3
+            else:
+                return False
+        elif encoded[i].startswith('11110'):
+            if i + 3 < len(encoded) and encoded[i + 1].startswith(
+                    '10') and encoded[i + 2].startswith('10') and encoded[
+                        i + 3].startswith('10'):
+                i += 4
+            else:
                 return False
         else:
-            if byte >> 6 != 0b10:
-                return False
-        num_bytes -= 1
-    return num_bytes == 0
+            return False
+    return True
